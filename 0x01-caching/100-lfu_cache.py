ca95ca24eb914de0beedbc128de81fe1bd2e44e7
sabah-hue <sabah.abdelbaset@gmail.com>
2024-08-22 05:55:32 +0300

    advanced

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" LFUCache class
+"""
+from base_caching import BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """  inherits from BaseCaching and is a caching system """
+
+    def put(self, key, item):
+        """ Must assign to the dictionary self.cache_data
+        the item value for the key key """
+        if key and item:
+            if len(self.cache_data.items()) > BaseCaching.MAX_ITEMS:
+                f_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", f_key)
+            else:
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """Must return the value in self.cache_data linked to key"""
+        # return self.cache_data.get(key, None)
+        if key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
