fc5cba9b3008e96d7e9d20e415ddfa4f6ef00d02
sabah-hue <sabah.abdelbaset@gmail.com>
2024-08-22 05:28:22 +0300

    basic class

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" BaseCaching class
+"""
+from basic_cache import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """  inherits from BaseCaching and is a caching system """
+
+    MAX_ITEMS = 0
+
+    def put(self, key, item):
+        """ Must assign to the dictionary self.cache_data
+        the item value for the key key """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Must return the value in self.cache_data linked to key"""
+        # return self.cache_data.get(key, None)
+        if key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
