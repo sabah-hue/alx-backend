064ba11deb35fad6d76219d5a0201b93f791ba5a
sabah-hue <sabah.abdelbaset@gmail.com>
2024-08-30 18:41:50 +0300

    gettext translated

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Basic Flask app """
+from flask import Flask, render_template
+from flask import g, request
+from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
+app.url_map.strict_slashes = False
+
+
+class Config:
+    """ config class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def hello_world():
+    """ call html file """
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ check match """
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
