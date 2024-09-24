660f989b062d7ec5da19128d2a1e7d6139d4c58c
sabah-hue <sabah.abdelbaset@gmail.com>
2024-09-24 18:09:21 +0300

    advanced redis

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -12,40 +12,13 @@ client.on('connect', () => {
   console.log('Redis client connected to the server');
 });
 
+client.hset('HolbertonSchools', {
+    'Portland': '50',
+    'Seattle': '80',
+    'New York': '20',
+    'Bogota': '20',
+    'Cali': '40',
+    'Paris': '2'
+})
 
-
-// client.hset('HolbertonSchools', {
-//     Portland: '50',
-//     Seattle: '80',
-//     'New York': '20',
-//     Bogota: '20',
-//     Cali: '40',
-//     Paris: '2'
-// })
-
-// let userSession = await client.hgetall('user-session:123');
-// console.log(JSON.stringify(userSession, null, 2));
-
-
-//  hash using hset
-const createHash = () => {
-  const hashKey = 'HolbertonSchools';
-  client.hset(hashKey, 'Portland', 50, print);
-  client.hset(hashKey, 'Seattle', 80, print);
-  client.hset(hashKey, 'New York', 20, print);
-  client.hset(hashKey, 'Bogota', 20, print);
-  client.hset(hashKey, 'Cali', 40, print);
-  client.hset(hashKey, 'Paris', 2, print);
-};
-
-// display hash
-const displayHash = () => {
-  const hashKey = 'HolbertonSchools';
-  client.hgetall(hashKey, (err, data) => {
-    if (err) console.error(err);
-    console.log(data);
-  });
-};
-
-createHash();
-displayHash();
+let userSession = client.hgetall('HolbertonSchools', (e, data)=>{console.log(data)});
